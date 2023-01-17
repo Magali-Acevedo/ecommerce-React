7ac5ec14d1c0ee685cf6cf0b1ef30cdc1314bb55
Magali <magiiee@live.com.ar>
2023-01-17 10:15:19 -0300

    Iniciando proyecto

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+//Componentes
+import BannerTop from "./components/BannerTop";
+import Header from "./components/Header";
+//Estilos
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <BannerTop></BannerTop>
+    <Header></Header>
+    
+    </>
+  )
+}
+
+export default App
